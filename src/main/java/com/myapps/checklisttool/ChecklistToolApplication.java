a84d3dfff15d238d61a4db69ebd554a832723a96
brian-mckeown <bmckeown2011@gmail.com>
2023-09-13 09:11:55 -0400

    initial commit

---
+++ src/main/java/com/myapps/checklisttool/ChecklistToolApplication.java
@@ -0,0 +1,13 @@
+package com.myapps.checklisttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecklistToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChecklistToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapps/checklisttool/ChecklistToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapps.checklisttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChecklistToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
