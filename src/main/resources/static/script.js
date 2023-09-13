9f0dcbbe097ceee96a7de0fc825022bc6b4f4009
brian-mckeown <bmckeown2011@gmail.com>
2023-09-13 12:13:11 -0400

    added flight buttons and initial angular logic

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,44 @@
+document.getElementById('send-button').addEventListener('click', sendMessage);
+document.getElementById('chat-message').addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {  // Enter key
+        event.preventDefault();  // Prevent default behavior like newline in textarea
+        sendMessage();
+    }
+});
+
+function sendMessage() {
+    const messageArea = document.getElementById('chat-message');
+    const chatContent = document.getElementById('chat-content');
+    const message = messageArea.value.trim();
+
+    if (message !== "") {
+        const timestamp = new Date().toLocaleTimeString();
+        chatContent.innerHTML += `<p><strong>[${timestamp}]</strong>: ${message}</p>`;
+        messageArea.value = "";  // Clear the textarea
+    }
+    // Scroll to the bottom of the chatContent
+    chatContent.scrollTop = chatContent.scrollHeight;
+}
+
+/**************************** */
+/*** ANGULAR JS SECTION BELOW */
+/**************************** */
+var app = angular.module('checklistApp', []);
+
+app.controller('ChecklistController', ['$scope', '$sce', function($scope, $sce) {
+    $scope.state = 'Idle';
+    $scope.messages = [];
+
+    $scope.setState = function(newState) {
+        $scope.state = newState;
+    };
+
+    $scope.startWalkaround = function() {
+        var timestamp = new Date().toLocaleTimeString(); // e.g., "12:35:47 PM"
+        
+        // Append messages
+        $scope.messages.push({ text: $sce.trustAsHtml("<strong>[" + timestamp + "]</strong>: Let's begin the walkaround"), color: 'magenta' });
+        $scope.messages.push({ text: $sce.trustAsHtml("A - Begin"), color: 'success' });
+        $scope.messages.push({ text: $sce.trustAsHtml("D - Cancel"), color: 'danger' });
+    };
+}]);
