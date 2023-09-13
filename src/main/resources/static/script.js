ae954bdd699b14fd54b60b6b21413aaa65df3a8f
brian-mckeown <bmckeown2011@gmail.com>
2023-09-13 14:08:01 -0400

    scroll to bottom issue fix

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,35 +1,21 @@
-/*document.getElementById('send-button').addEventListener('click', sendMessage);
-document.getElementById('chat-message').addEventListener('keypress', function(event) {
-    if (event.keyCode === 13 || event.which === 13) {  // Enter key
-        event.preventDefault();  // Prevent default behavior like newline in textarea
-        sendMessage();
-    }
-});
-
-function sendMessage() {
-    const messageArea = document.getElementById('chat-message');
-    const chatContent = document.getElementById('chat-content');
-    const message = messageArea.value.trim();
-
-    if (message !== "") {
-        const timestamp = new Date().toLocaleTimeString();
-        chatContent.innerHTML += `<p><strong>[${timestamp}]</strong>: ${message}</p>`;
-        messageArea.value = "";  // Clear the textarea
-    }
-    // Scroll to the bottom of the chatContent
-    chatContent.scrollTop = chatContent.scrollHeight;
-}*/
-
 /**************************** */
 /*** ANGULAR JS SECTION BELOW */
 /**************************** */
 var app = angular.module('checklistApp', []);
 
-app.controller('ChecklistController', ['$scope', '$sce', function($scope, $sce) {
+app.controller('ChecklistController', ['$scope', '$sce', '$timeout', function($scope, $sce, $timeout) {
     $scope.state = 'Idle';
     $scope.messages = [];
 
     $scope.walkaroundStarted = false;
+
+    $scope.buttonMessages = {
+        'Walkaround': {
+            'A': { text: 'A - Begin', color: 'success' },
+            'D': { text: 'D - Cancel', color: 'danger' }
+        },
+        // More states can be added here with their own button messages
+    };
 
     $scope.setState = function(newState) {
         $scope.state = newState;
@@ -44,6 +30,34 @@ app.controller('ChecklistController', ['$scope', '$sce', function($scope, $sce) 
         var trustedHtmlMessage = $sce.trustAsHtml("<strong>[" + timestamp + "]</strong>: " + $scope.chatMessage);
         $scope.messages.push({ text: trustedHtmlMessage, color: '' });
         $scope.chatMessage = '';  // Clear the message box
+        $scope.scrollToBottom(); // Scroll to the bottom after sending a message
+    };
+
+    $scope.scrollToBottom = function() {
+        $timeout(function() {
+            var chatContentDiv = document.getElementById('chat-content');
+            chatContentDiv.scrollTop = chatContentDiv.scrollHeight;
+        });
+    };
+
+    $scope.handleButtonPress = function(button) {
+        if ($scope.state in $scope.buttonMessages && button in $scope.buttonMessages[$scope.state]) {
+            var timestamp = new Date().toLocaleTimeString();
+            var message = $scope.buttonMessages[$scope.state][button].text;
+            var color = $scope.buttonMessages[$scope.state][button].color;
+            $scope.messages.push({ text: $sce.trustAsHtml("<strong>[" + timestamp + "]</strong>: " + message), color: color });
+        }
+    
+        if (button === 'D') {
+            if ($scope.state === 'Walkaround') {
+                $scope.walkaroundStarted = false;
+            }
+            $scope.state = 'Idle';
+        }
+    
+        // Additional actions for other buttons can be added as needed
+
+        $scope.scrollToBottom(); // Scroll to the bottom after sending a message
     };
 
     $scope.startWalkaround = function() {
@@ -55,5 +69,7 @@ app.controller('ChecklistController', ['$scope', '$sce', function($scope, $sce) 
         $scope.messages.push({ text: $sce.trustAsHtml("<strong>[" + timestamp + "]</strong>: Let's begin the walkaround"), color: 'magenta' });
         $scope.messages.push({ text: $sce.trustAsHtml("A - Begin"), color: 'success' });
         $scope.messages.push({ text: $sce.trustAsHtml("D - Cancel"), color: 'danger' });
+
+        $scope.scrollToBottom(); // Scroll to the bottom after sending a message
     };
 }]);
