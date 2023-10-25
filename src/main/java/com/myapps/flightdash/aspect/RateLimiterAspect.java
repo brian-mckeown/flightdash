110200f8b72ca319b2e812bb35a0a05722abf650
brian-mckeown <bmckeown2011@gmail.com>
2023-10-25 18:08:32 -0400

    Rate Limiting API Endpoints added

---
+++ src/main/java/com/myapps/flightdash/aspect/RateLimiterAspect.java
@@ -0,0 +1,32 @@
+package com.myapps.flightdash.aspect;
+
+import io.github.bucket4j.*;
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+import java.time.Duration;
+
+@Aspect
+@Component
+public class RateLimiterAspect {
+
+    private final Bucket bucket;
+
+    public RateLimiterAspect() {
+        Refill refill = Refill.greedy(20, Duration.ofMinutes(1));
+        Bandwidth limit = Bandwidth.classic(20, refill).withInitialTokens(1);
+        this.bucket = Bucket4j.builder().addLimit(limit).build();
+    }
+
+    @Around("@annotation(org.springframework.web.bind.annotation.RequestMapping) || @annotation(org.springframework.web.bind.annotation.GetMapping) || @annotation(org.springframework.web.bind.annotation.PostMapping)")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (bucket.tryConsume(1)) {
+            return joinPoint.proceed();
+        } else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("We are currently experiencing high traffic volume. Please try your request again in a couple of minutes");
+        }
+    }
+}
