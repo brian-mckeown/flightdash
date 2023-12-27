3aa4e6b0fea92b3abe164ff248de7497be36151c
brian-mckeown <bmckeown2011@gmail.com>
2023-12-27 09:49:15 -0500

    removed welcome popup

--- src/main/resources/static/vanilla-js.js
+++ src/main/resources/static/vanilla-js.js
@@ -21,52 +21,6 @@ window.onload = function() {
     toastList.forEach(toast => toast.show());
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Show the welcome modal
-    var modal = new bootstrap.Modal(document.getElementById('welcomeModal'), {
-      keyboard: false
-    });
-    modal.show();
-
-    // Warning message and overlay logic
-    let alertEl = document.getElementById('warning-message');
-    let overlayEl = document.getElementById('overlay');
-
-    // Show overlay when the alert is open and the screen width is less than 1200px
-    if (window.innerWidth < 1200) {
-        overlayEl.style.display = 'block';
-    }
-
-    // Add an event listener for the close button on the alert
-    alertEl.addEventListener('closed.bs.alert', function () {
-        overlayEl.style.display = 'none';
-    });
-
-    // Initialize popovers (ai costs)
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-    popoverTriggerList.forEach(function (popoverTriggerEl) {
-        new bootstrap.Popover(popoverTriggerEl, {
-            sanitize: false
-        });
-    });
-
-    // Add global click event listener for closing popovers
-    document.addEventListener('click', function (event) {
-        // Check if the clicked element is the close button inside a popover
-        if (event.target.matches('.close-popover')) {
-            // Find all popovers
-            var popovers = document.querySelectorAll('[data-bs-toggle="popover"]');
-            // Hide all popovers
-            popovers.forEach(function(popover) {
-                var bootstrapPopover = bootstrap.Popover.getInstance(popover);
-                if (bootstrapPopover) {
-                    bootstrapPopover.hide();
-                }
-            });
-        }
-    }, false);
-});
-
 fetch('/privacy-policy')
     .then(response => response.text())
     .then(content => {
